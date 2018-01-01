e7e1f2a2ec97759f604d78e0b2706bb610e426cb
Nghialm <nghialm83@gmail.com>
2018-01-01 13:17:52 +0700

    webservice update

---
+++ src/Syn/UpdateWC/UpdateWC/FileVersion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace UpdateWC
+{
+    [Serializable]
+    public class FileVersion
+    {
+        public string Folder
+        { get; set; }
+
+        public string File
+        { get; set; }
+
+        public string FileName
+        {
+            get;set;
+        }
+
+        public string Version
+        { get; set; }
+
+        public FileVersion()
+        { }
+
+        public FileVersion(string folder, string file, string version)
+        {
+            Folder = folder;
+            File = file;
+            FileName = Folder + (string.IsNullOrEmpty(Folder) ? @"\" : "") + File;
+            Version = version;
+        }
+    }
+}
